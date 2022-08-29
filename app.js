9ac9bafb0acb5e4e767a6b9be5161f1cce4ea96f
Jupiter Desphy <jupiterdesphy7@gmail.com>
2022-08-29 12:05:23 -0700

    Promises complete

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  *
  * @returns A promise that is designed to resolve with a list of hobbits, or potentially fail with an failure object. The failure object includes a boolean success property and a string message property.
  */
-function getList() {
+async function getList() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       let potentialFail = Math.round(Math.random() * 100) < 10;
@@ -15,10 +15,33 @@ function getList() {
   });
 }
 
-// TODO: Handle the resolved or rejected states of the promise
+// async function f1() {
+//   const x = await;
+//   console.log(x);
+// }
 
+// f1()
+
+// TODO: Handle the resolved or rejected states of the promise
 // TODO: If the promise resolves with the list of hobbits
 // Render the list of hobbits as list items within the unordered list with id="list" (check the index.html file)
+let feedbackPara = document.querySelector("#error");
+let ul = document.querySelector ("#list");
 
+getList()
+
+  .then((list) => {
+    console.log (list);
+    list.forEach(hobbit => {``
+      let li = document.createElement("li");
+      li.textContent = hobbit;
+      ul.appendChild(li);
+    });
+
+  })
+  .catch((err) => {
+    console.error(err);
+    feedbackPara.textContent = err.message;
+  });
 // TODO: If the promise rejects with the failure object
 // Display the failure message in the paragraph element with id="error" (check index.html file)
